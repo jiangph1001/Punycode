e5321f907b518a72f379c9c098c8ed4588b98a46
jiangph1001 <jiangph1001@foxmail.com>
2021-04-20 16:43:44 +0800

    change to python3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # coding=utf8
 
 red = "\033[0;31;1m"
@@ -9,7 +9,6 @@ cyan = "\033[0;36;1m"
 default = "\033[0m"
 
 print("\t Punycode 字符替换")
-print("\t VER007 https://github.com/ver007\n")
 
 key = {
     "a": "а",
@@ -42,21 +41,21 @@ key = {
     "z": "z",  # ENG
 }
 try:
-    ascii = str(raw_input(cyan + "输入要转换为的ASCII字符串(不区分大小写,非符号):" + green))
+    ascii = str(input(cyan + "输入要转换为的ASCII字符串(不区分大小写,非符号):" + green))
     ascii = ascii.lower()
-    print red + " ASCII: " + cyan + ascii, magenta + "转换后的 Unicode (Cyrillic-西里尔文) 如下: " + cyan
+    print(red + " ASCII: " + cyan + ascii, magenta + "转换后的 Unicode (Cyrillic-西里尔文) 如下: " + cyan)
     if ("f" in ascii) or ("g" in ascii) or ("u" in ascii) or ("v" in ascii) or ("z" in ascii):
-        print "ASCII 中包含 存在明显差异的字符串，这些字符串将不进行替换，如：f，g，u，v，z"
+        print("ASCII 中包含 存在明显差异的字符串，这些字符串将不进行替换，如：f，g，u，v，z")
 
     I = 0
     while I < len(ascii):
         if key.get(ascii[I]):
             ens = ascii.replace(ascii[I], key.get(ascii[I]))
-            print (" ASCII[" + magenta + "%s,hex(0x%s)" + cyan + "]->NewCode[" + magenta + "%s,hex(0x%s)" + cyan + "]") \
-                  % (ascii[I], ascii[I].encode("hex"), key.get(ascii[I]), key.get(ascii[I]).encode("hex")),
-            print ascii.replace(ascii[I], red + key.get(ascii[I]) + cyan),"[Domain:xn--%s]" % ens.encode('punycode')
+            print((" ASCII[" + magenta + "%s,hex(%s)" + cyan + "]->NewCode[" + magenta + "%s,hex(%s)" + cyan + "]") 
+            % (ascii[I], hex(ord(ascii[I])), key.get(ascii[I]), hex(ord(key.get(ascii[I])))),end=' ')
+            print(ascii.replace(ascii[I], red + key.get(ascii[I]) + cyan),"[Domain:xn--%s]" % ens.encode('punycode').decode('utf-8'))
         I += 1
 
     print(default + "\n")
-except:
+except Exception as e:
     print("\n\n" + red + "转换错误:请输入英文字母中的字符." + default + "\n")
--- produce.py
+++ produce.py
@@ -11,30 +11,29 @@ cyan = "\033[0;36;1m"
 default = "\033[0m"
 
 print("\t Punycode 字符生成")
-print("\t VER007 https://github.com/ver007")
-
 
 def hand(name, start, end, output):
     _start, _end = int(start, 16), int(end, 16)
     for i in range(_start, _end):
         try:
-            unicode_str = "\u" + str(hex(i))[2:].zfill(4)
+            unicode_str = str(hex(i))[2:].zfill(4).encode('utf-8')
+            #print(unicode_str)
             new_str = unicode_str.decode("unicode_escape")
             if new_str.strip() != "":
-                output.write("[%s]" % name.encode("utf8") + "-" + unicode_str + "-")
-                output.write(new_str.encode("utf8"))
+                output.write("[%s]" % name.encode("utf8") + "-" + unicode_str.decode('utf-8') + "-")
+                output.write(new_str)
                 output.write("\n")
                 output.flush()
         except Exception as e:
-            print e
+            print(e)
 
 
 list_key = json.loads(open("./unicode.json", "r").read())
 
-print "输出字符表..."
-for i in list_key.keys():
-    key, val = list_key[i]
-    output = open("./out/%s.txt" % i.replace(" ",""), "w")
-    print "\t 输入表: %s" % i.encode("utf8")
-    hand(i, key, val, output)
+print("输出字符表...")
+for key in list_key.keys():
+    start, end = list_key[key]
+    output = open("./out/%s.txt" % key.replace(" ",""), "w")
+    print("\t 输入表: %s" % key.encode("utf8"))
+    hand(key, start, end, output)
     output.close()
